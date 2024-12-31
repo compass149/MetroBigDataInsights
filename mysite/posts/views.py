8d9969d572016d42a97572f951449d98e776eacb
compass149 <a01055821627@gmail.com>
2024-12-31 14:17:05 +0900

    first commit

---
+++ mysite/posts/views.py
@@ -0,0 +1,48 @@
+#컨트롤러 역할  - request, response 담당
+from django.http import HttpResponse
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Posts
+
+from django.contrib import messages
+from .form import PostCreateForm
+
+#게시글 등록
+# def create_post(request):
+#     return HttpResponse('게시글 등록')
+def create_post(request):
+    form = PostCreateForm()
+    
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            messages.success(request, '게시글이 등록되었습니다.')
+            return redirect("posts:read", post_id = post.id)
+        else:
+            messages.error(request, '게시글 등록에 실패했습니다.')
+    return render(request, 'posts/create.html', {'form': form})
+
+#게시글 보기
+#def get_post(request, post_id):
+#    return HttpResponse('게시글 보기')
+def get_post(request, post_id):
+    post = get_object_or_404(Posts, id= post_id)
+    return render(request, 'posts/read.html', {'post':post})
+
+#게시글 수정
+def update_post(request, post_id):
+    return HttpResponse('게시글 수정')
+
+#게시글 삭제
+def delete_post(request, post_id):
+    return HttpResponse('게시글 삭제')
+
+#게시글 목록
+# def get_posts(request):
+#     return HttpResponse('게시글 목록')
+def get_posts(request):
+    posts = Posts.objects.all().order_by('-created_at')
+    return render(request, 'posts/list.html', {'posts': posts})
