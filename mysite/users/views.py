6733b9f9125504d4d2886590d1a56dd76639da69
compass149 <Githu>
2025-01-24 00:13:32 +0900

    [conf].gitinore 추가

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,33 +1,8 @@
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required, user_passes_test
-
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db.models import Q
 from django.core.paginator import Paginator
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required, user_passes_test
-
-# 사용자 목록
-@login_required(login_url='auth:login')
-@user_passes_test(lambda u: u.is_superuser)
-def get_users(request):
-    return HttpResponse('사용자 목록')
-
-# 사용자 보기
-@login_required(login_url='auth:login')
-@user_passes_test(lambda u: u.is_superuser)
-def get_user(request, user_id):
-    return HttpResponse('사용자 보기')
-
-# 사용자 삭제
-@login_required(login_url='auth:login')
-@user_passes_test(lambda u: u.is_superuser)
-def delete_user(request, user_id):
-    return HttpResponse('사용자 삭제')
-
 
 # 사용자 목록
 @login_required(login_url='auth:login')
@@ -42,25 +17,49 @@ def get_users(request):
     if searchType not in [None, ''] and searchKeyword not in [None, '']:
         if searchType == 'all':
             users = users.filter(
-                Q(username__contains=searchKeyword) |
-                Q(first_name__contains=searchKeyword) |
-                Q(email__contains=searchKeyword)
+                Q(username__icontains=searchKeyword) |
+                Q(first_name__icontains=searchKeyword) |
+                Q(email__icontains=searchKeyword)
             )
+        elif searchType == 'username':
+            users = users.filter(username__icontains=searchKeyword)
+        elif searchType == 'first_name':
+            users = users.filter(first_name__icontains=searchKeyword)
+        elif searchType == 'email':
+            users = users.filter(email__icontains=searchKeyword)
 
-    # 페이징네이션
-    paginator = Paginator(users, 10)
+    # 페이지네이션
+    paginator = Paginator(users, 10)  # 페이지당 10명
     page_obj = paginator.get_page(page)
 
-    # 현재 페이지의 첫 번째 사용자 번호 계산
+    # 사용자 리스트에 순번 추가
     start_index = paginator.count - (paginator.per_page * (page_obj.number - 1))
-
-    # 순번 계산하여 사용자 리스트에 추가
-    for index, u in enumerate(page_obj, start=0):
-        page_obj[index].index_num = start_index - index
+    users_with_index = [
+        {
+            'index_num': start_index - idx,
+            'user': user
+        }
+        for idx, user in enumerate(page_obj)
+    ]
 
     return render(request, 'users/list.html', {
-        'users': page_obj,
+        'users': users_with_index,
         'searchType': searchType,
-        'searchKeyword': searchKeyword
+        'searchKeyword': searchKeyword,
+        'paginator': paginator,
+        'page_obj': page_obj,
     })
 
+
+# 사용자 보기
+@login_required(login_url='auth:login')
+@user_passes_test(lambda u: u.is_superuser)
+def get_user(request, user_id):
+    return HttpResponse('사용자 보기')
+
+
+# 사용자 삭제
+@login_required(login_url='auth:login')
+@user_passes_test(lambda u: u.is_superuser)
+def delete_user(request, user_id):
+    return HttpResponse('사용자 삭제')
