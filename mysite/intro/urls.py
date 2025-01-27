6eb8b419f8d4cbaac451fd675bf9890ad0194ef1
compass149 <Githu>
2025-01-28 00:01:47 +0900

    [feat]각 페이지 이동 완료.

--- mysite/intro/urls.py
+++ mysite/intro/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),        # 메인 페이지
     path('member/', views.member, name='member'),  # 팀원 소개 페이지
     path('data_source/', views.data_source, name='data_source'),  # 데이터 소개 페이지
+    path('chart/', views.chart, name='chart'),  # 데이터 소개 페이지
 ]
