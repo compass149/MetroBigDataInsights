9e418f51221c26a5ed4375e35a49c0d5cc4b0897
compass149 <Githu>
2025-01-28 00:01:47 +0900

    [feat]각 페이지 이동 완료.

--- mysite/intro/views.py
+++ mysite/intro/views.py
@@ -4,7 +4,10 @@ def index(request):
     return render(request, 'intro/index.html')
 
 def member(request):
-    return render(request, 'intro/member.html')
+    return render(request, 'intro/member.html',{'page_title': '팀원 소개 Page'})
 
 def data_source(request):
-    return render(request, 'intro/data_source.html')
+    return render(request, 'intro/data_source.html',{'page_title': '활용 데이터'})
+
+def chart(request):
+    return render(request, 'intro/chart.html',{'page_title': '지하철 혼잡도 차트'})
