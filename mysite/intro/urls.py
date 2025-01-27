a64aca18f1f95b549ad15b87c046ff467fc9c327
compass149 <a01055821627@gmail.com>
2025-01-27 23:00:34 +0900

    [conf]gitignore에 캐시파일 추가

--- mysite/intro/urls.py
+++ mysite/intro/urls.py
@@ -5,4 +5,5 @@ app_name = 'intro'
 urlpatterns = [
     path('', views.index, name='index'),        # 메인 페이지
     path('member/', views.member, name='member'),  # 팀원 소개 페이지
+    path('data_source/', views.data_source, name='data_source'),  # 데이터 소개 페이지
 ]
